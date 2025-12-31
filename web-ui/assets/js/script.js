d98540944ad015264cff53e149d7d166de491571
Luis Hernandez <43943030+LuisHernandez20@users.noreply.github.com>
2025-12-31 09:31:06 -0600

    Creacion de interfaz ui de usuario
    Se realiza la creación de la beta ui para la interacción del usuario con el resultado final

---
+++ web-ui/assets/js/script.js
@@ -0,0 +1,41 @@
+ const ctx = document.getElementById('grafica1');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+   const ctx2 = document.getElementById('grafica2');
+
+  new Chart(ctx2, {
+    type: 'pie',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
